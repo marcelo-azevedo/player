1bc9ac7db87a507c56136165707523a9d5cad5fc
vmotors <VMotors@MacBook-Pro-de-Marcelo.local>
2013-08-12 20:49:59 -0300

    comentando linas do js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-$(document).ready(function(){
-	$("#slide img:eq(0)").addClass("ativo").show();
+// $(document).ready(function(){
+// 	$("#slide img:eq(0)").addClass("ativo").show();
 
-	setInterval(slide,7000);
+// 	setInterval(slide,7000);
 
-	function slide(){
-		if($(".ativo").next().size()){
-			$(".ativo").fadeOut().removeClass("ativo").next().fadeIn().addClass("ativo");
-		}else{
-			$(".ativo").fadeOut().removeClass("ativo");
-			$("#slide img:eq(0)").fadeIn().addClass("ativo");
-		}
+// 	function slide(){
+// 		if($(".ativo").next().size()){
+// 			$(".ativo").fadeOut().removeClass("ativo").next().fadeIn().addClass("ativo");
+// 		}else{
+// 			$(".ativo").fadeOut().removeClass("ativo");
+// 			$("#slide img:eq(0)").fadeIn().addClass("ativo");
+// 		}
 
-	}
-});
+// 	}
+// });
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
 	
 
 
-});
+// });
 
 
 // $(document).ready(function(){
